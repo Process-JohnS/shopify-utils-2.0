0e811e7493694262fa8099098e2affbef3ab692f
Process-JohnS <john.sevilla@processcreative.com.au>
2020-08-17 17:20:09 +1000

    Added generator examples

--- src/iterators/iterator.ts
+++ src/iterators/iterator.ts
@@ -5,13 +5,9 @@ export class ResourceIterator<T> implements Iterator<T> {
   constructor(public components: T[]) {}
 
   public next(): IteratorResult<T> {
-    return this.pointer < this.components.length ? {
-      done: false,
-      value: this.components[this.pointer++]
-    } : {
-      done: true,
-      value: null
-    };
+    return this.pointer < this.components.length
+      ? { done: false, value: this.components[this.pointer++] }
+      : { done: true, value: null };
   }
 
   public [Symbol.iterator] = (): IterableIterator<T> => this;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -18,3 +18,74 @@ export const splitCSVLine = (line: string) => {
 export const getSimilarityRating = (a: string, b: string): number => {
   return levenshtein.get(a, b);
 }
+
+
+
+
+
+
+
+/* Generators */
+
+
+const getArrayGenerator = (arr: any[]) => function* (start: any = 0, end: any = arr.length): IterableIterator<any> {
+  if (start === undefined) return arr;
+  if (end === undefined && start > -1) {
+    end = start - 1;
+    start = 0;
+  }
+  if (end === 'tail') return arr.slice(start);
+  if (start > -1) end = end + 1;
+  yield* arr.slice(start, end);
+};
+
+
+
+class SimpleGenerator {
+
+  * generateNumber(count: number): IterableIterator<number> {
+    while(true)
+      yield count++;
+  }
+
+  * generateString(): IterableIterator<string> {
+    yield 'one';
+    yield 'two';
+    yield 'three';
+    yield 'four';
+    yield 'five';
+  }
+
+  * generateArray(items: any[]): IterableIterator<string> {
+    while (true) yield* items;
+  }
+
+}
+
+
+
+const generatorDemo = () => {
+
+  let simpleGenerator = new SimpleGenerator();
+  let stringGenerator = simpleGenerator.generateString();
+  let numberGenerator = simpleGenerator.generateNumber(10);
+
+  let arr = [
+    {one:1, two:2, three:3},
+    {one:1, two:2, three:3},
+    {one:1, two:2, three:3},
+    {one:1, two:2, three:3},
+  ]
+
+  let arrayGenerator = getArrayGenerator(arr)();
+  let next = arrayGenerator.next();
+  for (let i = 0; i < 20; i++) {
+    if (next.done) break;
+    console.log(next.value);
+    next = arrayGenerator.next();
+  }
+
+}
+
+
+
