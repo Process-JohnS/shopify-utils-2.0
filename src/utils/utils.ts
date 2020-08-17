f0619e8a2a4dc39b7535e2d10895e0bedc8db94b
Process-JohnS <john.sevilla@processcreative.com.au>
2020-08-17 16:07:15 +1000

    Added some parsing utilities

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,20 @@
 
+import split from 'split-string';
+import * as levenshtein from 'fast-levenshtein';
+
+
+
 export const wait = (ms: number) => {
   let start = new Date().getTime(), end = start;
   while (end < start + ms) end = new Date().getTime();
 };
+
+export const splitCSVLine = (line: string) => {
+  return split(line, { separator: ',', quotes: ['"'], keep: (value: string, state: any) => {
+    return value !== '\\' && (value !== '"' || state.prev() === '\\');
+  }});
+}
+
+export const getSimilarityRating = (a: string, b: string): number => {
+  return levenshtein.get(a, b);
+}
