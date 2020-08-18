be1778f8ace93a87a08a056770f4b490adefb140
Process-JohnS <john.sevilla@processcreative.com.au>
2020-08-18 17:53:32 +1000

    Clone repo tool and package.json downloader

---
+++ src/other/git-tools/clone-repo.ts
@@ -0,0 +1,114 @@
+
+
+import { Clone, Cred, Repository } from 'nodegit';
+import * as fs from 'fs-extra';
+import { existsSync } from 'fs-extra';
+
+const username = 'Process-JohnS';
+const password = 'nrR?Hrx3*CP4';
+
+const protocol = 'https';
+const gitDomain = 'github.com';
+const gitOwner = 'Process-Creative';
+
+
+
+export enum DeployType {
+  YML = 'config.yml',
+  ENV = '.env'
+};
+
+export type DeployParams = {
+  shopName: string,
+  shopPassword: string,
+  themeId: string
+};
+
+
+
+
+let cloneOptions = {
+  fetchOpts: {
+    callbacks: {
+      credentials: () => {
+        let userpassPlaintextNew = Cred.userpassPlaintextNew
+        return userpassPlaintextNew(username, password);
+      }
+    }
+  }
+}
+
+
+
+export const cloneRepo = async (repoName: string) => {
+  let repo: Repository;
+
+  if (fs.existsSync(repoName)) {
+    console.error('Clone directory already exists');
+    return repo;
+  }
+
+  let gitUrl = `${protocol}://${gitDomain}/${gitOwner}/${repoName}.git`;
+
+  try {
+    repo = await Clone.clone(gitUrl, repoName, cloneOptions);
+    console.log(`${repoName} cloned`);
+  }
+  catch (e) {
+    switch (e.errno) {
+      case -4:
+        console.error(`Error: Output directory ${repoName} already exists`);
+        break;
+      case -1:
+        console.error(`Error: Remote authentication required`);
+        break;
+      default:
+        console.error(e);
+    }
+  }
+
+  return repo;
+}
+
+
+
+
+
+export const createDeployFile = (type: DeployType, { shopName, shopPassword, themeId }: DeployParams) => {
+  let deployFileContents: string;
+
+  switch (type) {
+
+    case DeployType.ENV:
+      deployFileContents =
+      `SLATE_STORE=${shopName}.myshopify.com\n` +
+      `SLATE_PASSWORD=${shopPassword}\n` +
+      `SLATE_THEME_ID=${themeId}\n` +
+      `SLATE_IGNORE_FILES=src/config/settings_data.json\n`;
+      break;
+
+    case DeployType.YML:
+      deployFileContents =
+      `development:\n` +
+      `  password: ${shopPassword}\n` +
+      `  theme_id: ${themeId}\n` +
+      `  store: ${shopName}.myshopify.com\n` +
+      `ignore_files:\n` +
+      `    - settings_data.json\n`
+      break;
+
+    default:
+      console.log('Error: Unknown deploy type');
+
+  }
+
+  let deployFilePath = shopName + '/' + type;
+
+  if (!existsSync(deployFilePath)) {
+    fs.writeFileSync(deployFilePath, deployFileContents);
+    console.log(`${type} file created successfully`);
+  } else {
+    console.error(`Error: ${type} file already exists`);
+  }
+
+}
